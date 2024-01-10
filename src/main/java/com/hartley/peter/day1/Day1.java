9c7b1fef2c70edc194350c49136b664ad0ce572d
Peter Hartley <peter.hartley@skybettingandgaming.com>
2024-01-10 13:49:47 +0000

    Initial commit and day 1 solutions

---
+++ src/main/java/com/hartley/peter/day1/Day1.java
@@ -0,0 +1,29 @@
+package com.hartley.peter.day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Day1 {
+
+    public static void main(String[] args) throws IOException {
+        int result = 0;
+
+        InputStream inputStream = Day1.class.getResourceAsStream("/day1");
+        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
+        for (String line; (line = reader.readLine()) != null;) {
+            result += getCalibrationValue(line);;
+        }
+
+        System.out.println("Result = " + result);
+    }
+
+    private static Integer getCalibrationValue(String value) {
+        String cleanedString = value.replaceAll("[A-Za-z]+", "");
+        return Integer.parseInt("" + (cleanedString.charAt(0)) + (cleanedString.charAt(cleanedString.length() - 1)));
+    }
+
+}
---
+++ src/main/java/com/hartley/peter/day1/Day1b.java
@@ -0,0 +1,67 @@
+package com.hartley.peter.day1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+
+public class Day1b {
+
+    public static void main(String[] args) throws IOException {
+        int result = 0;
+
+        InputStream inputStream = Day1.class.getResourceAsStream("/day1");
+        InputStreamReader streamReader = new InputStreamReader(inputStream, StandardCharsets.UTF_8);
+        BufferedReader reader = new BufferedReader(streamReader);
+        for (String line; (line = reader.readLine()) != null;) {
+            result += getCalibrationValue(line);;
+        }
+
+        System.out.println("Result = " + result);
+    }
+
+    private static Integer getCalibrationValue(String value) {
+        String cleanedString = convertCalibrationValue(value);
+        return Integer.parseInt("" + (cleanedString.charAt(0)) + (cleanedString.charAt(cleanedString.length() - 1)));
+    }
+
+    private static String convertCalibrationValue(String value) {
+        String calibration = "";
+        String slice = value;
+
+        while(!slice.isEmpty()) {
+            calibration += Character.isDigit(slice.charAt(0)) ? slice.charAt(0) : findNumber(slice);
+            slice = slice.substring(1);
+        }
+
+        return calibration;
+    }
+
+    private static final String[] numbers = {
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+    };
+
+    private static String findNumber(String value) {
+        String number = "";
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (value.startsWith(numbers[i])) {
+                number = Integer.toString(i + 1);
+            }
+        }
+
+        return number;
+    }
+
+
+
+}
